78035cae041a8fd77608806380fd7e22a15d12e2
syntax <dlscam112@gmail.com>
2025-07-07 03:21:51 +0500

    inf: ..

--- frontend/src/components/molecules/optionsNav.tsx
+++ frontend/src/components/molecules/optionsNav.tsx
@@ -1,23 +1,51 @@
+import { isOpenStore } from '../../store/isOpen'
+
 export function OptionsNav() {
+	const handleClick = () => {
+		isOpenStore.setIsOpen('menuMobile', false)
+	}
+
 	return (
-		<div className='block md:flex  gap-10'>
-			<a className='block md:flex text-center' href='#About_me'>
+		<div onClick={handleClick} className='block md:flex  gap-10'>
+			{/* ********* */}
+			<a
+				onClick={handleClick}
+				className='block md:flex text-center'
+				href='#About_me'
+			>
 				про меня
 			</a>
 
-			<a className='block md:flex mt-4 md:mt-0 text-center' href='#Stack'>
+			<a
+				onClick={handleClick}
+				className='block md:flex mt-4 md:mt-0 text-center'
+				href='#Stack'
+			>
 				стек
 			</a>
 
-			<a className='block md:flex mt-4 md:mt-0 text-center' href='#Contacts'>
+			<a
+				onClick={handleClick}
+				className='block md:flex mt-4 md:mt-0 text-center'
+				href='#Contacts'
+			>
 				контакты
 			</a>
 
-			<a className='block md:flex mt-4 md:mt-0 text-center' href='#projects'>
+			<a
+				onClick={handleClick}
+				className='block md:flex mt-4 md:mt-0 text-center'
+				href='#projects'
+			>
 				проекты
 			</a>
 
-			<a className='block md:flex mt-4 md:mt-0 text-center'>закрыть</a>
+			<a
+				onClick={handleClick}
+				className='block md:flex mt-4 md:mt-0 text-center'
+			>
+				закрыть
+			</a>
 		</div>
 	)
 }
