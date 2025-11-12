5bf6caf32f0d11aecfbd0902915d78c26a9c121d
Zhanbolat Tanauov <dlscam112@gmail.com>
2025-11-12 11:11:35 +0500

    HUI

--- frontend/src/components/organisms/stack.tsx
+++ frontend/src/components/organisms/stack.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { useState } from "react";
+
 import {
   SiCss3,
   SiHtml5,
@@ -14,8 +14,6 @@ const sizeIcon = 50;
 
 export function Stack() {
   const cls = "transition-all el cp flex";
-  const [hover, setHover] = useState(false);
-  const [hoveredElId, setHoveredElId] = useState<number>();
 
   const els = [
     {
@@ -65,42 +63,17 @@ export function Stack() {
     },
   ];
 
-  const handleOnMouseEl = (elID: number) => {
-    setHover(true);
-    setHoveredElId(elID);
-  };
-  const handleOnLeaveEl = () => {
-    setHover(false);
-    setHoveredElId(0);
-  };
-
   return (
     <div
-      className="mx-auto transition duration-900 bg-[#333]/20 py-10 text-center mt-20 rtl"
+      className="mx-auto transition duration-900 bg-[#333]/10 py-10 text-center mt-20 rtl"
       id="Stack"
     >
       <h3>стек</h3>
 
       <div className="flex flex-wrap gap-10 w-[90%] md:w-[70%] justify-between mx-auto mt-10 py-5 px-5 rounded-4xl">
         {els.map((e, idx) => (
-          <div
-            onMouseEnter={() => handleOnMouseEl(e.id)}
-            onMouseLeave={() => handleOnLeaveEl()}
-            key={idx}
-            className={clsx(
-              cls,
-              "gap-3",
-              hover &&
-                hoveredElId == e.id &&
-                "bg-white/10 rounded-2xl transition duration-900 p-4 align-center grow-0 share-0"
-            )}
-          >
+          <div key={idx} className={clsx(cls, "gap-3")}>
             {e.icon}
-
-            <div className="block transition duration-900 max-w-[300px]">
-              <h5>{hover && e.id === hoveredElId ? e.title : null}</h5>
-              <p>{hover && e.id === hoveredElId ? e.description : null}</p>
-            </div>
           </div>
         ))}
       </div>
